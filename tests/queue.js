8657431b9374f8cd02315286472b0e0b363376c7
Seva Maltsev <sevamaltsev@gmail.com>
2018-05-05 18:45:40 +0300

    Continuing to create mvp

--- tests/queue.js
+++ tests/queue.js
@@ -1,17 +1,56 @@
+const chai = require('chai');
+
 const { Queue } = require('../lib');
+
+const expect = chai.expect;
 
 const queueConfig = {
   connectionUrls: ['url'],
-  queueConfig: {},
+  queueConfig: {
+    queues: {
+      queueOne: {
+        ack: true,
+      },
+      queueTwo: {
+        ack: false,
+      },
+    },
+  },
+};
+
+const createQueueStub = (config = queueConfig) => {
+  const queueHandler = new Queue(config);
+  Object.values(queueHandler).map(queue => {
+    queue.simulateMessage = function (data) {
+      const message = this.createMessageObj(data);
+      this.onMessage(message);
+    }
+  });
 }
 
 describe('Queue', () => {
   it('Should not throw on create', () => {
-    const queue = new Queue(queueConfig);
+    const queueHandler = new Queue(queueConfig);
   });
-  it('Should have queues property that contains all configured queues');
-  describe('.queues.someQueue', () => {
-    it('Should have ack and nack when configured');
+  it('Should have queues property that contains all configured queues', () => {
+    const queueHandler = new Queue(queueConfig);
+    expect(queueHandler.queues).to.have.all.keys('queueOne', 'queueTwo');
+  });
+  describe('.queues.someQueue message', () => {
+    it('Should recieve messages using the onMessage funciton', () => {
+      const queue = createQueueStub().queues.queueOne;
+      let m;
+      queue.onMessage((message) => { m = message; });
+      queue.simulateMessage({ hello: true });
+      expect(m).to.be.an('object').and.have.all.keys('hello');
+
+    });
+    it('Should have ack and nack when configured', () => {
+      const queueHandler = createQueueStub();
+      queueHandler.queues.queueOne.simulateMessage({ hello: true });
+      queueHandler.queues.queueOne.ack.to.be.an.instanceof('function');
+      queueHandler.queues.queueOne.nack.to.be.an.instanceof('function');
+    });
     it('Should throw if ack or nack is called when not configured');
     it('Should call worker() when a message is found');
     describe('worker()', () => {
